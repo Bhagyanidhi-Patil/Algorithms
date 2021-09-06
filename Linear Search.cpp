0d90dd5d6b893e6edc0fe87f4621707d86853b32
Bhagyanidhi Patil <66917223+Bhagyanidhi-Patil@users.noreply.github.com>
2021-09-06 22:40:55 +0530

    Update Linear Search.cpp

--- Linear Search.cpp
+++ Linear Search.cpp
@@ -1,16 +1,31 @@
-include <iostream>
+#include <iostream>
 using namespace std;
-int main() {
-   int sea, c, n=6;
-   int arr[] = { 12, 35, 69, 74, 165, 54};
-   sea=165;
-   for (c = 0; c < n; c++) {
-      if (arr[c] == sea) {
-         printf("%d is present at location %d.\n", search, c+1);
-         break;
-      }
-   }
-   if (c == n)
-      printf("%d isn't present in the array.\n", search);
-   return 0;
+
+void linearsearch(int a[],int n)
+{
+    int temp=-1;
+    for(int i=0;i<5;i++)
+    {
+        if(a[i] == n)
+        {
+            cout << "element is found at "<<i+1<<endl;
+            temp=0;
+            break;
+        }
+    }
+    if(temp!=0)
+    {
+        cout<<" Element not found"<<endl;
+    }
+}
+int main()
+{
+    int ar[5],n;
+    for(int i=0;i<5;i++)
+    {
+        cin >>ar[i];
+    }
+    cin >>n;
+    linearsearch(ar,n);
+    return 0;
 }
